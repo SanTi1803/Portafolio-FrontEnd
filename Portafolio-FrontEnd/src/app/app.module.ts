04b920cc8064aba35d4510cefcd719c554316e42
Santiago Morales <santiago.morales2003@gmail.com>
2022-12-15 10:40:58 -0300

    Sección educación y trabajo terminado

--- Portafolio-FrontEnd/src/app/app.module.ts
+++ Portafolio-FrontEnd/src/app/app.module.ts
@@ -8,6 +8,10 @@ import { BannerComponent } from './components/banner/banner.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { SocialComponent } from './components/social/social.component';
+import { ExperenciaComponent } from './components/experencia/experencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +20,15 @@ import { SocialComponent } from './components/social/social.component';
     BannerComponent,
     LogoApComponent,
     AcercaDeComponent,
-    SocialComponent
+    SocialComponent,
+    ExperenciaComponent,
+    EducacionComponent,
+    HysComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
