8b483dd3410019b917717da4a24f7e08e41364da
Santiago Morales <santiago.morales2003@gmail.com>
2022-12-16 11:26:45 -0300

    sección hys

---
+++ Portafolio-FrontEnd/src/app/components/hys/hys.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hys',
+  templateUrl: './hys.component.html',
+  styleUrls: ['./hys.component.css']
+})
+export class HysComponent {
+}
